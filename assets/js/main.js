ada28d9b2486088a1e5b10ec3acc8a92c90238f5
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-04-20 01:54:58 +0700

    p

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,14 +103,3 @@ function removeScale() {
 }
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
-
-const image = document.querySelector(".home__img");
-const resume = document.querySelector("body");
-
-image.addEventListener("mouseenter", () => {
-  resume.classList.add("blur"); // Blur hanya ke bagian resume
-});
-
-image.addEventListener("mouseleave", () => {
-  resume.classList.remove("blur"); // Kembali normal
-});
