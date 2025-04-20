10486579537040aa1f295fd5fbc1ab5725c670ea
Andera Singgih Pratama <anderasinggih1@gmail.com>
2025-04-20 13:05:00 +0700

    p

--- assets/js/main.js
+++ assets/js/main.js
@@ -125,15 +125,3 @@ function handleScroll() {
     }
   });
 }
-
-const skillBoxes = document.querySelectorAll(".skills__box");
-
-skillBoxes.forEach((box) => {
-  box.addEventListener("click", () => {
-    box.classList.add("clicked");
-
-    setTimeout(() => {
-      box.classList.remove("clicked");
-    }, 100); // Sesuai dengan transition time
-  });
-});
